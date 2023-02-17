de0da718c5b2191b1ceb39fa52978cf6f65ade81
ITFangyz <2463185230@qq.com>
2023-02-17 19:16:03 +0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+
+// const app = createApp(App)
+
+// app.use(ElementPlus)
 createApp(App).use(router).mount('#app')
+// createApp(App).use(router).use(ElementPlus).mount('#app')
